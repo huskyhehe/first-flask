225ea2ea0aab4ca353e612c4a3bfab4659bf30e2
huskyhehe <zhou.he1@northeastern.edu>
2023-01-04 18:10:39 -0800

    Add tests/conftest.py which config tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,8 +6,8 @@ import pytest
 from flaskr import create_app
 from flaskr.db import init_db, get_db
 
-with open(os.path.join(os.path.dirname(__file__), 'data.sql'), 'rb') as f:
-    _data_sql = f.read().decode('utf8')
+with open(os.path.join(os.path.dirname(__file__), "data.sql"), "rb") as f:
+    _data_sql = f.read().decode("utf8")
 
 
 @pytest.fixture
@@ -26,8 +26,8 @@ def app():
     })
 
     '''
-    TESTING tells Flask that the app is in test mode. 
-    Flask changes some internal behavior so it’s easier to test, 
+    TESTING tells Flask that the app is in tests mode. 
+    Flask changes some internal behavior so it’s easier to tests, 
     and other extensions can also use the flag to make testing them easier.
     
     The DATABASE path is overridden 
@@ -44,8 +44,8 @@ def app():
     os.unlink(db_path)
 
     '''
-    After setting the path, the database tables are created and the test data is inserted. 
-    After the test is over, the temporary file is closed and removed.
+    After setting the path, the database tables are created and the tests data is inserted. 
+    After the tests is over, the temporary file is closed and removed.
     '''
 
 
